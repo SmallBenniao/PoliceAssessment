f3ed319c24989a55cc9f24d17eb20d8e28cb0093
MyAndroidStore <2019880507@qq.com>
2018-09-10 09:56:37 +0800

    1.0.1.2

--- app/src/main/java/com/police/assessment/policeassessment/fragment/AssessmentFragment.java
+++ app/src/main/java/com/police/assessment/policeassessment/fragment/AssessmentFragment.java
@@ -1,30 +1,51 @@
 package com.police.assessment.policeassessment.fragment;
 
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.police.assessment.policeassessment.R;
+import com.police.assessment.policeassessment.base.fragment.BaseFragment;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * 考核模块
  */
-public class AssessmentFragment extends Fragment {
+public class AssessmentFragment extends BaseFragment {
 
 
-    public AssessmentFragment() {
-        // Required empty public constructor
+    Unbinder unbinder;
+
+    @Override
+    public int getLayoutResId() {
+        return R.layout.fragment_assessment;
+    }
+
+    @Override
+    public void init(View rootView) {
+        unbinder = ButterKnife.bind(this, rootView);
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_assessment, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 
+    @OnClick({R.id.day, R.id.month, R.id.record, R.id.integral})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.day:
+                break;
+            case R.id.month:
+                break;
+            case R.id.record:
+                break;
+            case R.id.integral:
+                break;
+        }
+    }
 }
